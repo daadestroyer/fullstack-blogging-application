1615bc06b846f6695c362dce24841c4c10a7472e
daadestroyer <nigamshubham2000@gmail.com>
2022-07-24 13:20:43 +0530

    backend structure ready...

---
+++ 3-backend/blogging-application-backend/src/main/java/com/blogging/backend/BloggingApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blogging.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApplicationBackendApplication.class, args);
+	}
+
+}
